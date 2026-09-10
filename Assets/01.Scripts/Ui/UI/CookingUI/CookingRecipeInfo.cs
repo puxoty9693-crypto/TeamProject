e7a616e8f9d418b086ba0d060c360b736ed618c4
namgang98 <ox2950@gmail.com>
2026-09-11 08:55:31 +0900

    /commit/cookinguifix

--- Assets/01.Scripts/Ui/UI/CookingUI/CookingRecipeInfo.cs
+++ Assets/01.Scripts/Ui/UI/CookingUI/CookingRecipeInfo.cs
@@ -4,12 +4,23 @@ using UnityEngine.UI;
 
 public class CookingRecipeInfo : MonoBehaviour
 {
+    [Header ("재료와음식정보")]
     [SerializeField] Image ingredient1Image1;
     [SerializeField] Image ingredient1Image2;
     [SerializeField] Image foodImage;
     [SerializeField] TextMeshProUGUI ingredient1Count;
     [SerializeField] TextMeshProUGUI ingredient2Count;
+    
     //[SerializeField] Button cookingButton; 나중에 요리하기 만드시면 이걸로 연결해주세요
+
+    [Header("숫자업다운")]
+    [SerializeField] Button plus1Btn;
+    [SerializeField] Button plus5Btn;
+    [SerializeField] Button plus10Btn;
+    [SerializeField] Button minus1Btn;
+    [SerializeField] Button minus5Btn;
+    [SerializeField] Button minus10Btn;
+    [SerializeField] TextMeshProUGUI countText;
 
     public void UpdateCookingInfo(RecipeData data)
     {
