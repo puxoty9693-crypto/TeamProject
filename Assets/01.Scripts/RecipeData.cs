fae10380153fb50291ed70835a12a859c9d95b4d
jd1kwh <75837870+jd1kwh@users.noreply.github.com>
2026-09-15 17:48:57 +0900

    commit

--- Assets/01.Scripts/RecipeData.cs
+++ Assets/01.Scripts/RecipeData.cs
@@ -4,17 +4,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Recipe_", menuName = "Data/Recipe")]
 public class RecipeData : ScriptableObject
 {
-<<<<<<< HEAD
+
     public string recipeId;      // ë ˆì‹œí”¼ ê³ ìœ  ID
     public string recipeName;    // ~ ë ˆì‹œí”¼
     public int unlockGoldCost;   // í•´ê¸ˆì— í•„ìš”í•œ ê³¨ë“œ
     public FoodData food;        // ì—°ê²°ëœ ìŒì‹
-
-=======
-    public string recipeId;      // ·¹½ÃÇÇ °íÀ¯ ID
-    public string recipeName;    // ~ ·¹½ÃÇÇ
-    public int unlockGoldCost;   // ÇØ±Ý¿¡ ÇÊ¿äÇÑ °ñµå
-    public FoodData food;        // ¿¬°áµÈ À½½Ä
->>>>>>> Develop
     public float cookingTime;
 }
