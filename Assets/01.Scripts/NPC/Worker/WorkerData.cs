b58f42bf6ade728ee943007cf4231b6d3aa7be32
puxoty9693-crypto <puxoty9693@gmail.com>
2026-09-16 11:18:07 +0900

    Revert "워커 수정"
    This reverts commit 65ad5c77e2962cb7cb24ae2c08bc8a87f723dc6a.

--- Assets/01.Scripts/NPC/Worker/WorkerData.cs
+++ Assets/01.Scripts/NPC/Worker/WorkerData.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-// NPC 한 명의 기본 정보
+
 [CreateAssetMenu(fileName = "Worker_", menuName = "Data/NPC/Worker")]
 
 public class WorkerData : NPCData
@@ -8,7 +8,4 @@ public class WorkerData : NPCData
     [SerializeField] private WorkerStats baseStats = new();
 
     public WorkerStats BaseStats => baseStats;
-    public WorkerRole role;      // Worker 역할
-    public string workerName;    // Worker 이름
-    public Sprite workerImage;   // Worker 이미지
 }
