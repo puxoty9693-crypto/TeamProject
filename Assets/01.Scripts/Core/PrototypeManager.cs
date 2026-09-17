8339bfadd91dde1766a8e75c8124c265bebfc0c0
jd1kwh <75837870+jd1kwh@users.noreply.github.com>
2026-09-17 15:07:12 +0900

    commit

---
+++ Assets/01.Scripts/Core/PrototypeManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PrototypeManager : MonoBehaviour
+{
+    FoodData testFood = null;
+    void Start()
+    {
+        SaveManager.Instance.DeleteSave();
+        testFood = DataManager.Instance.allFoods[0];
+        for(int i = 0; i < 10; i++) Debug.Log(GameManager.Instance.PaymentSystem.Pay(testFood));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(SaveManager.Instance.CurrentData.Gold);
+    }
+}
