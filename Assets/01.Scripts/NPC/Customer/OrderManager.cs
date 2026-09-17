5bf760cceea0382f50b57c018ef054f898af983d
jalstn96-svg <jalstn96@gmail.com>
2026-09-17 14:01:18 +0900

    0917_temp

--- Assets/01.Scripts/NPC/Customer/OrderManager.cs
+++ Assets/01.Scripts/NPC/Customer/OrderManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class OrderManager : MonoBehaviour
+public class OrderManager : MMSingleton<OrderManager>
 {
     private readonly List<Customer> orderList = new();
 
